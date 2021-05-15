c7c82ebfd39fd91c5d84d1fb5edcea94dc49b6ff
makicamel <unright@gmail.com>
2021-05-15 21:58:17 +0900

    Describe current year and month

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:intl/date_symbol_data_local.dart';
+
 import 'components/day.dart';
 
 void main() {
+  initializeDateFormatting('ja_JP', null).then((_) => {});
   runApp(TakeCare());
 }
 
 class TakeCare extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final title = DateFormat.yM('ja_JP').format(DateTime.now());
     return MaterialApp(
       title: 'Take Care',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Calendar(title: 'Calendar'),
+      home: Calendar(title: title),
     );
   }
 }
