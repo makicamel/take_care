ed5243585001a0da626132683d0b8d9c2fa01c30
makicamel <unright@gmail.com>
2021-06-12 12:52:46 +0900

    Now we can switch month

--- lib/screens/calendar_screen.dart
+++ lib/screens/calendar_screen.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:date_calc/date_calc.dart';
+import 'package:intl/intl.dart';
+
 import '../widgets/month.dart';
 
 class CalendarScreen extends StatefulWidget {
   CalendarScreen({Key key, this.title}) : super(key: key);
 
-  final String title;
+  String title;
 
   @override
   _CalendarScreenState createState() => _CalendarScreenState();
 }
 
 class _CalendarScreenState extends State<CalendarScreen> {
+  var referenceDate = DateCalc.now();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,11 +24,40 @@ class _CalendarScreenState extends State<CalendarScreen> {
         child: Container(
           child: Column(
             children: [
-              Expanded(child: Month(DateTime.now())),
+              Expanded(child: Month(referenceDate)),
+              Row(
+                children: [
+                  Spacer(),
+                  ElevatedButton(
+                    child: const Text('Prev'),
+                    onPressed: () => _addMonth(),
+                  ),
+                  Spacer(),
+                  ElevatedButton(
+                    child: const Text('Next'),
+                    onPressed: () => _subtractMonth(),
+                  ),
+                  Spacer(),
+                ],
+              ),
             ],
           ),
         ),
       ),
     );
+  }
+
+  void _addMonth() {
+    setState(() {
+      referenceDate = referenceDate.subtractMonth(1);
+      widget.title = DateFormat.yM('ja_JP').format(referenceDate);
+    });
+  }
+
+  void _subtractMonth() {
+    setState(() {
+      referenceDate = referenceDate.addMonth(1);
+      widget.title = DateFormat.yM('ja_JP').format(referenceDate);
+    });
   }
 }
